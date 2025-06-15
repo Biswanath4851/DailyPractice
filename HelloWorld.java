11b62158c04e4fb3ee98e8cd96453fdaddd05ee2
Biswanath4851 <156915677+Biswanath4851@users.noreply.github.com>
2025-06-15 18:27:08 +0000

    test

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    // Entry point of the Java program
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Codespaces!");
+    }
+}
